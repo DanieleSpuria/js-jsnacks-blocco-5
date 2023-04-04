88a11ab882566e8f68a4ca8d741b257273432ff9
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-04-04 15:59:55 +0200

    add utenti

---
+++ jSnack1/script.js
@@ -0,0 +1,42 @@
+const utenti = [
+  {
+    nome: 'Filippo',
+    cognome: 'Mangialupi',
+    eta: 18,
+  }
+  {
+    nome: 'Giovanni',
+    cognome: 'Barbaro',
+    eta: 21,
+  }
+  {
+    nome: 'Dario',
+    cognome: 'La Spina',
+    eta: 14,
+  }
+  {
+    nome: 'Angela',
+    cognome: 'Civiletti',
+    eta: 70,
+  }
+  {
+    nome: 'Giuseppe',
+    cognome: 'Ortiga',
+    eta: 66,
+  }
+  {
+    nome: 'Gertrude',
+    cognome: 'Raballo',
+    eta: 50,
+  }
+  {
+    nome: 'Anna',
+    cognome: 'Manna',
+    eta: 80,
+  }
+  {
+    nome: 'Romoaldo',
+    cognome: 'Ortense',
+    eta: 10,
+  }
+]
