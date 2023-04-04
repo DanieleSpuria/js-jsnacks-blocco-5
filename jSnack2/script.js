94a6729508a9cfbe097fe0c31ca02405bec9cf2d
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-04-04 16:13:59 +0200

    add studenti

---
+++ jSnack2/script.js
@@ -0,0 +1,46 @@
+const studenti = [
+  {
+    nome: 'Giancarlo',
+    cognome: 'Alessi',
+    matricola: '521469',
+    voti: {
+      matematica: 7,
+      informatica: 6,
+      fisica: 8,
+      chimica: 5,
+    }
+  },
+  {
+    nome: 'Alessia',
+    cognome: 'Giancarli',
+    matricola: '154896',
+    voti: {
+      matematica: 8,
+      informatica: 8,
+      fisica: 8,
+      chimica: 8,
+    }
+  },
+  {
+    nome: 'Onesta',
+    cognome: 'Fasulli',
+    matricola: '219845',
+    voti: {
+      matematica: 6,
+      informatica: 3,
+      fisica: 10,
+      chimica: 5,
+    }
+  },
+  {
+    nome: 'Filippo',
+    cognome: 'Privitera',
+    matricola: '486159',
+    voti: {
+      matematica: 9,
+      informatica: 5,
+      fisica: 3,
+      chimica: 1,
+    }
+  },
+]
