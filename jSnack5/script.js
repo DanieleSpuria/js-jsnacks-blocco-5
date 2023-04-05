b5a883948a7ea53f8b44cb03a3eff804c4c7eb01
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-04-05 15:45:58 +0200

    add bikes

---
+++ jSnack5/script.js
@@ -0,0 +1,30 @@
+const bikes = [
+  {
+    name: 'gianni',
+    weigth: 10
+  },
+  {
+    name: 'filippo',
+    weigth: 15
+  },
+  {
+    name: 'carmela',
+    weigth: 18
+  },
+  {
+    name: 'anselmo',
+    weigth: 16
+  },
+  {
+    name: 'gianfilippo',
+    weigth: 13
+  },
+  {
+    name: 'amalda',
+    weigth: 9
+  },
+  {
+    name: 'montagna',
+    weigth: 18
+  }
+]
