4d61d90277591d0e73d39eb9d258f258dc87bdca
DanieleSpuria <daniele.spuria.jama@gmail.com>
2023-04-05 17:41:43 +0200

    add teams, getRandomNumber

---
+++ jSnack6/script.js
@@ -0,0 +1,47 @@
+const teams = [
+  {
+    nome: 'Inter',
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: 'Roma',
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: 'Juventus',
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: 'Messina',
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: 'Napoli',
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: 'Palermo',
+    puntiFatti: 0,
+    falliSubiti: 0
+  },
+  {
+    nome: 'Camaro',
+    puntiFatti: 0,
+    falliSubiti: 0
+  }
+]
+
+let getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+teams.forEach((team) => {
+  team.puntiFatti = getRandomNumber(1,100);
+  team.falliSubiti = getRandomNumber(1,100);
+});
+
+
+console.log(teams);
